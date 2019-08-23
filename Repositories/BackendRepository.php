a1c49373044a7a8d00429600eeefd85f6b089f82
marco76tv <marco.sottana@gmail.com>
2019-08-23 23:12:56 +0200

    .

---
+++ Repositories/BackendRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace Modules\Backend\Repositories;
+
+//---base
+use Modules\Xot\Repositories\XotBaseRepository;
+
+class BackendRepository extends XotBaseRepository{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    protected $model = 'Modules\Backend\Models\Backend';
+}
